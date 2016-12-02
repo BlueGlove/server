212070115724c0654f03cfd5ca7c6c21725b6df3
Andy Jiang <andyjiang@gmail.com>
2016-12-01 20:21:50 -0800

    updated server/db-consumer.js

--- server/db-consumer.js
+++ server/db-consumer.js
@@ -14,28 +14,19 @@ exports.add = function *add(consumerNickname) {
   var consumerPK = 'cus_' + uuid.v4();
   var q = 'INSERT INTO bluegloves.consumer (consumerPK, consumerNickname) ';
   q += 'VALUES (\'' + consumerPK + '\', \'' + consumerNickname + '\');';
-
   console.log(q);
-
-  return yield thunkedQuery(q);
-
-  // thunkedQuery(q)(function(err, res) {
-  //   console.log(err);
-  //   console.log(res);
-  // });
-
-  // return res;
+  return yield db.thunkedQuery(q);
 };
 
-function thunkedQuery(q) {
-  return function(fn) {
-    db.connect(function(err, client, done) {
-      client.query(q, [], function(err, res) {
-        done();
-        if (err) console.error(err);
-        console.log(res);
-        fn();
-      });
-    });
-  }
-}
+// function thunkedQuery(q) {
+//   return function(fn) {
+//     db.connect(function(err, client, done) {
+//       client.query(q, [], function(err, res) {
+//         done();
+//         if (err) console.error(err);
+//         console.log(res);
+//         fn();
+//       });
+//     });
+//   }
+// }
