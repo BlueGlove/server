3641d189473ea31d3a28ba0564d1989385589698
Andy Jiang <andyjiang@gmail.com>
2016-12-01 20:37:15 -0800

    updated server/db-visit.js

--- server/db-visit.js
+++ server/db-visit.js
@@ -18,22 +18,5 @@ exports.add = function *add(beaconId, iosId, visitBeginTime, visitEndTime) {
   var q = 'INSERT INTO bluegloves.visit (beaconID, iosID, visitBeginTime, visitEndTime) ';
   q += 'VALUES (\'' + beaconId + '\', \'' + iosId + '\', \'' + visitBeginTime + '\', \'' + visitEndTime + '\');';
   console.log(q);
-  return yield db.thunkedQuery(q);
+  return yield db(q);
 };
-
-/**
- * Thunked query.
- */
-
-// function thunkedQuery(q) {
-//   return function(fn) {
-//     db.connect(function(err, client, done) {
-//       client.query(q, [], function(err, res) {
-//         done();
-//         if (err) console.error(err);
-//         console.log(res);
-//         fn();
-//       });
-//     });
-//   }
-// }
