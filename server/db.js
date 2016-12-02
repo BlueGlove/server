a6ba7f779d924cf0c3e8c8ceb43c10abaec2695e
Andy Jiang <andyjiang@gmail.com>
2016-12-01 20:33:50 -0800

    updated server/db.js

--- server/db.js
+++ server/db.js
@@ -16,35 +16,26 @@ var emitter = new EventEmitter();
 emitter.setMaxListeners(0);
 
 /**
- * Expose `Db`.
+ * Configure Pool.
  */
 
-module.exports = Db;
+var pool = new Pool({
+  user: config.pgUser,
+  password: config.pgPassword,
+  host: config.pgHost,
+  database: config.pgName,
+  max: 20, // max number of clients in pool
+  idleTimeoutMillis: 1000, // close & remove clients which have been idle > 1 second
+  ssl: true
+});
 
 /**
- * Initialize `Db`
+ * Expose thunked query function.
  */
 
-function Db() {
-  if (!(this instanceof Db)) return new Db();
-  this.pool = new Pool({
-    user: config.pgUser,
-    password: config.pgPassword,
-    host: config.pgHost,
-    database: config.pgName,
-    max: 20, // max number of clients in pool
-    idleTimeoutMillis: 1000, // close & remove clients which have been idle > 1 second
-    ssl: true
-  });
-}
-
-/**
- * ThunkedQuery.
- */
-
-Db.prototype.thunkedQuery = function(q) {
+module.exports = function(q) {
   return function(fn) {
-    Db.pool.connect(function(err, client, done) {
+    pool.connect(function(err, client, done) {
       client.query(q, [], function(err, res) {
         done();
         if (err) console.error(err);
@@ -53,4 +44,4 @@ Db.prototype.thunkedQuery = function(q) {
       });
     });
   }
-}
+};
