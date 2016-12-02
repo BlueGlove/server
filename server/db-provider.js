50747e77376f532082deb825e6710ba4fee39331
Andy Jiang <andyjiang@gmail.com>
2016-12-01 20:21:50 -0800

    updated server/db-provider.js

--- server/db-provider.js
+++ server/db-provider.js
@@ -19,17 +19,19 @@ exports.add = function *add(providerOrgName, providerEmail) {
   var providerPK = 'pro_' + uuid.v4();
   var q = 'INSERT INTO bluegloves.provider (providerPK, providerOrgName, providerEmail) ';
   q += 'VALUES (\'' + providerPK + '\', \'' + providerOrgName + '\', \'' + providerEmail + '\');';
-
   console.log(q);
-
-  var res = yield thunkedQuery(q);
-  return res;
+  return yield thunkedQuery(q);
 };
 
 function thunkedQuery(q) {
   return function(fn) {
-    db.query(q, function(err, res) {
-      db.end(fn);
+    db.connect(function(err, client, done) {
+      client.query(q, [], function(err, res) {
+        done();
+        if (err) console.error(err);
+        console.log(res);
+        fn();
+      });
     });
   }
 }
